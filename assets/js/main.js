7e6a57c27ce39ccd3ab7ffb63b7d7c4af71a4fba
aliimrandu <aimran583@gmail.com>
2017-06-04 16:58:59 +0600

    3

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    $(".videoplay-button").magnificpopup({
+        type:"video";
+    })
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
